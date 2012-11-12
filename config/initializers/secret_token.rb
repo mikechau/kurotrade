b70690b0a1104823678fc5aa409ddd9919bf5286
mikechau <mchau@iwu.edu>
2012-11-12 13:53:25 -0600

    First commit, base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kurotrade::Application.config.secret_token = '86341a9d8325031134538731a7b9aa27f321fed40abcd7f6b0ca894c668cc684cf809451ccdba5c7b79bd32b04c0b391329e0b35184f4bae67e783598aed38e5'
