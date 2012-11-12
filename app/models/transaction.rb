3efd70ac4e9ea9550abfd5b242697ca09a637442
mikechau <mchau@iwu.edu>
2012-11-12 16:36:01 -0600

    Scaffold Transaction

---
+++ app/models/transaction.rb
@@ -0,0 +1,3 @@
+class Transaction < ActiveRecord::Base
+  attr_accessible :broker, :commission, :description, :fees, :group_id, :interest, :price, :quantity, :settle_date, :stock_symbol, :total_value, :trade_date, :trade_id
+end
