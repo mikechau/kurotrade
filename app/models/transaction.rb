4a88cefe8ac4a2564675aca6b6dfcf0ae74d6ca3
mikechau <mchau@iwu.edu>
2012-11-14 14:28:26 -0600

    Established model associations

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,3 +1,5 @@
 class Transaction < ActiveRecord::Base
   attr_accessible :broker, :commission, :description, :fees, :group_id, :interest, :price, :quantity, :settle_date, :stock_symbol, :total_value, :trade_date, :trade_id
+
+  belongs_to :group
 end
