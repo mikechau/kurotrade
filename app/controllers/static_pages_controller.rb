99af988cd0ceb87e08f54544d40ef04c67d2857a
mikechau <mchau@iwu.edu>
2012-11-26 04:04:11 -0600

    Add charts: morris.js, raphael.js, added experimental page: balances for nav calc

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,4 +3,20 @@ class StaticPagesController < ApplicationController
 	def index
 	end
 
+	def balance_exp
+		@transactions_initial = Transaction.order(:trade_date)
+		@transactions = @transactions_initial.where(:user_id => 1)
+
+		# @initial_nav_value = 0
+		# @initial_nav_units = 1000
+	end
 end
+
+# NAV UNITS: 
+#   WITHDRAWAL: #PAYMENT ON ACCCOUNT
+# Previous NAV - ( -Withdrawl / [ ( Cash + Stock Value - Withdrawal ) / Previous NAV) ] )
+
+#1000 - (-Withdrawl / [])
+
+#   DEPOSIT: #DEPOSIT CKS
+# 	Previous NAV + ( Deposit / [ ( Cash + Stock Value - Deposit ) / Previous NAV) ] )
