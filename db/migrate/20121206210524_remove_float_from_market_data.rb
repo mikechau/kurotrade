251c9656c6bf2bef936ee36f6fbd2d5985319c40
mikechau <mchau@iwu.edu>
2012-12-09 16:25:52 -0600

    Remove old migrations

--- db/migrate/20121206210524_remove_float_from_market_data.rb
+++
@@ -1,9 +0,0 @@
-class RemoveFloatFromMarketData < ActiveRecord::Migration
-  def up
-    remove_column :market_data, :float
-  end
-
-  def down
-    add_column :market_data, :float, :string
-  end
-end
