1301b1c5f2203be499929ac0a45e8d2e4718fb50
mikechau <mchau@iwu.edu>
2012-12-10 11:58:46 -0600

    Add styling and navigation

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_username(params[:username])
+
+    if user != nil && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to users_url, :flash => { :success => "kurotrade welcomes you #{user.name}!" } #change this
+    else
+      redirect_to login_url, :flash => { :error => "<b>Invalid login!</b> Please try again or #{view_context.link_to('register.', new_user_url)}".html_safe } #this is the login page
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to login_url, :flash => { :alert => "You are now logged out! Come back soon." }
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,18 @@
 Kurotrade::Application.routes.draw do
 
   root :to => 'static_pages#index'
+
+  #resource :session, :only => [:new, :create, :destroy]
+
+  get "login" => 'sessions#new', as: :login
+  post "session" => 'sessions#create' 
+  get "logout" => 'sessions#destroy', as: :logout
+
   get '/balances', :to => 'static_pages#balance_exp'
-  get '/dashboard', :to => 'static_pages#mock_dashboard'
+  get '/test', :to => 'static_pages#testing'
+  get '/dashboard', :to => 'static_pages#mock_dashboard', as: :dashboard
+  get '/about', :to => 'static_pages#about', as: :about
+  get '/contact', :to => 'static_pages#contact', as: :contact
 
   resources :transactions
   match '/transactions/file_upload', :to => 'transactions#scottrade_csv_parser', :as => 'transactions_upload'
